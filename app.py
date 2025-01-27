20f872b7592f693bf4ae20fac8248277a89cc718
Alex_Souza2002 <62069087+AlexdeSouza2502@users.noreply.github.com>
2025-01-27 19:58:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+st.header("Calculadora", delimiter="gray")
+
+
+st.write("Expressão")
+expression = st.text_input("Entre com os valores")
+
+if (expression):
+  result=eval(expression)
+
+  st.write("Resultado:", result)
